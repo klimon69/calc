39e4a4a12f0464d2670869e20e94b44a6764da5b
klimon <klim_fizik@bk.ru>
2018-03-05 14:55:01 +0300

    а

---
+++ js/script.js
@@ -0,0 +1,26 @@
+var Calc1;
+
+
+window.onload = function() {
+    Calc1=555
+    };
+
+
+
+
+
+
+
+
+
+function myFunction(clicked_id) {
+    
+
+    
+   alert(clicked_id);
+      alert(Calc1);
+      document.getElementById("output").innerHTML=splitText;
+   
+    
+   }
+
