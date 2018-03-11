83378c0bc24ba494dc6052e9db292fed318f2e12
klimon69 <34158447+klimon69@users.noreply.github.com>
2018-03-11 20:18:29 +0300

    sss

--- js/script.js
+++ js/script.js
@@ -1,26 +1,58 @@
-var Calc1;
-
-
-window.onload = function() {
-    Calc1=555
-    };
 
 
 
-
-
-
-
+var Num = [];
+var firstNum;
+var SecondNum;
 
 
 function myFunction(clicked_id) {
     
-
+      
     
-   alert(clicked_id);
-      alert(Calc1);
-      document.getElementById("output").innerHTML=splitText;
-   
+      var b = clicked_id;
+      
+      
+      Num.push(b);
+      
+
+      document.getElementById("numers").innerHTML = Num.join ('');
+      
     
    }
 
+   function myClear() {
+
+   	document.getElementById("numers").innerHTML = "0";
+   	Num = [];
+
+
+   }
+
+
+  function myPlus(){
+
+
+   var firstNum = document.getElementById("numers").innerHTML;
+   	
+   	document.getElementById("numers").innerHTML = "0";
+   	Num = [];
+   	
+    myFunction();
+
+    
+    alert(firstNum);
+  
+  }
+
+
+
+function myEqual(){
+
+  
+
+   var SecondNum = document.getElementById("numers").innerHTML;
+   document.getElementById("numers").innerHTML = "0";
+   document.getElementById("numers").innerHTML = Number(firstNum) + Number(SecondNum);
+
+}
