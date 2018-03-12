8ee936ad44587fcca5c1b360c5f65ac778fe5854
klimon69 <34158447+klimon69@users.noreply.github.com>
2018-03-12 22:23:35 +0300

    123

--- js/script.js
+++ js/script.js
@@ -5,6 +5,8 @@ var Num = [];
 //var firstNum;
 //var SecondNum;
 var myNumber = new Number("0");
+var SecondNum = new Number("0");
+var flag = new Number("0");
 
 
 
@@ -22,7 +24,7 @@ function myFunction(clicked_id) {
     
    }
 
-   function myClear() {
+ function myClear() {
 
    	document.getElementById("numers").innerHTML = "0";
    	Num = [];
@@ -38,12 +40,73 @@ function myFunction(clicked_id) {
    	
    myClear();
    		
-    myFunction();
+   myFunction();
 
     
    document.getElementById("numers").innerHTML = "0"; 
+   flag = 1;
   
   }
+
+
+
+
+
+  function myMinus(){
+
+
+   myNumber = document.getElementById("numers").innerHTML;
+   	
+   myClear();
+   		
+   myFunction();
+
+    
+   document.getElementById("numers").innerHTML = "0"; 
+
+   flag = 2;
+  
+  }
+
+function myDivide(){
+
+
+   myNumber = document.getElementById("numers").innerHTML;
+   	
+   myClear();
+   		
+   myFunction();
+
+    
+   document.getElementById("numers").innerHTML = "0"; 
+
+   flag = 3;
+  
+  }
+
+
+  
+
+  function myMultiply(){
+
+
+   myNumber = document.getElementById("numers").innerHTML;
+   	
+   myClear();
+   		
+   myFunction();
+
+    
+   document.getElementById("numers").innerHTML = "0"; 
+
+   flag = 4;
+  
+  }
+
+
+
+
+
 
 
 
@@ -53,6 +116,36 @@ function myEqual(){
 
    var SecondNum = document.getElementById("numers").innerHTML;
    document.getElementById("numers").innerHTML = "0";
+
+   alert(myNumber);
+   alert(SecondNum);
+
+
+
+   if (flag==1) {
+
    document.getElementById("numers").innerHTML = Number(myNumber) + Number(SecondNum);
+
+} else if (flag==2){
+
+	
+   document.getElementById("numers").innerHTML = myNumber - SecondNum;
+
+}  else if (flag==3){
+
+	
+   document.getElementById("numers").innerHTML = myNumber/SecondNum;
+
+}  else if (flag==4){
+
+	
+   document.getElementById("numers").innerHTML = myNumber*SecondNum;
+
+} 
+
+ 
+
+
+
 
 }
